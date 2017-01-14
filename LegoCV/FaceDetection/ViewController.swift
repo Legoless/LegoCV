df63a63d0ce6de8f81c4af9a4abd9fc5a70add50
Dal Rupnik <legoless@gmail.com>
2017-01-14 19:14:33 +0100

    Added Xcode project

---
+++ LegoCV/FaceDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Dal Rupnik on 14/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LegoCV/LegoCV/LegoCV.h
@@ -0,0 +1,19 @@
+//
+//  LegoCV.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 14/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LegoCV.
+FOUNDATION_EXPORT double LegoCVVersionNumber;
+
+//! Project version string for LegoCV.
+FOUNDATION_EXPORT const unsigned char LegoCVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LegoCV/PublicHeader.h>
+
+
