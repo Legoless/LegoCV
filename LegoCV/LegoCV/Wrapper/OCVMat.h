783de893313c0007c811dc8749c5211409b1efd0
Dal Rupnik <legoless@gmail.com>
2017-01-16 20:04:54 +0100

    Added initializers, changed wrapper to Objective-C++

--- LegoCV/LegoCV/Wrapper/OCVMat.h
+++ LegoCV/LegoCV/Wrapper/OCVMat.h
@@ -6,11 +6,26 @@
 //  Copyright © 2017 Unified Sense. All rights reserved.
 //
 
-@import Foundation;
+#import <Foundation/Foundation.h>
+
+#import "OCVSize.h"
 
 @interface OCVMat : NSObject
 
 - (instancetype)initWithRows:(NSInteger)rows cols:(NSInteger)cols type:(NSInteger)type;
+- (instancetype)initWithSize:(OCVSize *)size type:(NSInteger)type;
+- (instancetype)initWithMat:(OCVMat *)mat;
 
+@end
+
+//
+// MARK: Unavailable in Swift, convenience for OpenCV
+//
+
+#import <opencv2/core/mat.hpp>
+
+@interface OCVMat (OpenCV)
+
+- (cv::Mat)matInstance;
 
 @end
