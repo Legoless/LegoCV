70af01a0a85fa6771376ec974f59822e9cdf5d26
Dal Rupnik <legoless@gmail.com>
2017-01-16 20:38:21 +0100

    Added OCVImage class

---
+++ LegoCV/LegoCV/Wrapper/OCVImage.h
@@ -0,0 +1,13 @@
+//
+//  OCVImage.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 16/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCVImage : NSObject
+
+@end
