644fec0326f6dd533fa80056244811c2b8626be9
Dal Rupnik <legoless@gmail.com>
2017-01-18 20:29:47 +0100

    Added OCVObject and OCVOutputArray

---
+++ LegoCV/LegoCV/Wrapper/Utilities/OCVObject.h
@@ -0,0 +1,13 @@
+//
+//  OCVObject.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 18/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCVObject : NSObject
+
+@end
---
+++ LegoCV/LegoCV/Wrapper/Utilities/OCVObject.m
@@ -0,0 +1,13 @@
+//
+//  OCVObject.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 18/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVObject.h"
+
+@implementation OCVObject
+
+@end
