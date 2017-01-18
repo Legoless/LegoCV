a315eed23445fa730152186c3c78271c8b534bff
Dal Rupnik <legoless@gmail.com>
2017-01-18 21:05:15 +0100

    Added output array

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVInputOutputArray.h
@@ -0,0 +1,13 @@
+//
+//  OCVInputOutputArray.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 18/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVOutputArray.h"
+
+@interface OCVInputOutputArray : OCVOutputArray
+
+@end
---
+++ LegoCV/LegoCV/Wrapper/Core/OCVInputOutputArray.m
@@ -0,0 +1,13 @@
+//
+//  OCVInputOutputArray.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 18/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVInputOutputArray.h"
+
+@implementation OCVInputOutputArray
+
+@end
