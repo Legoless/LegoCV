4e2d5f4752fcaeb6326ea64e7dcaab9bc1199407
Dal Rupnik <legoless@gmail.com>
2017-01-22 18:25:45 +0100

    Added OCVRect

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVRect.m
@@ -0,0 +1,13 @@
+//
+//  OCVRect.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 22/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVRect.h"
+
+@implementation OCVRect
+
+@end
