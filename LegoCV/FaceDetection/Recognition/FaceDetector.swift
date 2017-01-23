0674959ebd36dedd157c2a55893103c56ebfcd50
Dal Rupnik <legoless@gmail.com>
2017-01-23 11:56:05 +0100

    Added face recognition classes

---
+++ LegoCV/FaceDetection/Recognition/FaceDetector.swift
@@ -0,0 +1,13 @@
+//
+//  FaceDetector.swift
+//  LegoCV
+//
+//  Created by Dal Rupnik on 23/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+import UIKit
+
+class FaceDetector: NSObject {
+
+}
