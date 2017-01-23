45f6bd6c5d6e20aca0b3740a6ac177ed76d032ed
Dal Rupnik <legoless@gmail.com>
2017-01-23 12:22:52 +0100

    Added camera classes

---
+++ LegoCV/LegoCV/Wrapper/Video/OCVVideoCamera.h
@@ -0,0 +1,13 @@
+//
+//  OCVVideoCamera.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 23/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCVVideoCamera : NSObject
+
+@end
---
+++ LegoCV/LegoCV/Wrapper/Video/OCVVideoCamera.m
@@ -0,0 +1,13 @@
+//
+//  OCVVideoCamera.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 23/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVVideoCamera.h"
+
+@implementation OCVVideoCamera
+
+@end
