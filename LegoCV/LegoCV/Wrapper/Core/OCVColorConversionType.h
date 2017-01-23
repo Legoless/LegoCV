61bc3b59d8b817575b5ddb52c58c474a5c6d04ce
Dal Rupnik <legoless@gmail.com>
2017-01-23 19:00:08 +0100

    Added OCVColorConversionType

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVColorConversionType.h
@@ -0,0 +1,13 @@
+//
+//  OCVColorConversionType.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 23/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#ifndef OCVColorConversionType_h
+#define OCVColorConversionType_h
+
+
+#endif /* OCVColorConversionType_h */
