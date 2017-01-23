79b7282ba3fa3cff193bd43e44cf7cfd2bbdb52e
Dal Rupnik <legoless@gmail.com>
2017-01-23 11:50:42 +0100

    Added view controllers for face recognition example

---
+++ LegoCV/FaceDetection/View Controllers/FaceRecognitionVC.swift
@@ -0,0 +1,35 @@
+//
+//  FaceRecognitionVC.swift
+//  LegoCV
+//
+//  Created by Dal Rupnik on 23/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+import UIKit
+
+class FaceRecognitionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
