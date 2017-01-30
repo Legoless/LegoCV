29dc44fe47f32168d3c99053b3073816ed308eb1
Dal Rupnik <legoless@gmail.com>
2017-01-30 14:59:11 +0100

    Added OCVScalar wrapper

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVScalar+Private.h
@@ -0,0 +1,31 @@
+//
+//  OCVScalar+Private.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 30/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVScalar.h"
+
+#import <opencv2/core/core.hpp>
+
+cv::cvScalar convertScalar(OCVScalar scalar) {
+    cv::cvScalar finalScalar;
+    finalScalar.val[0] = scalar.value[0];
+    finalScalar.val[1] = scalar.value[1];
+    finalScalar.val[2] = scalar.value[2];
+    finalScalar.val[3] = scalar.value[3];
+    
+    return finalScalar;
+}
+
+OCVScalar convertScalar(cv::cvScalar rect) {
+    OCVScalar finalScalar;
+    finalScalar.value[0] = scalar.val[0];
+    finalScalar.value[1] = scalar.val[1];
+    finalScalar.value[2] = scalar.val[2];
+    finalScalar.value[3] = scalar.val[3];
+    
+    return finalScalar;
+}
---
+++ LegoCV/LegoCV/Wrapper/Core/OCVScalar.h
@@ -0,0 +1,11 @@
+//
+//  OCVScalar.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 30/01/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+typedef struct OCVScalar {
+    double value[4];
+};
