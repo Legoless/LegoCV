0c7b8d8c03ca26932c0429946e860fd10e3bdc77
Dal Rupnik <legoless@gmail.com>
2017-03-04 19:54:30 +0100

    Added OCVTermCriteria

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVTermCriteria.h
@@ -0,0 +1,13 @@
+//
+//  OCVTermCriteria.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 04/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVObject.h"
+
+@interface OCVTermCriteria : OCVObject
+
+@end
