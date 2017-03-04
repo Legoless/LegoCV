c2dbf8f7a208406820165c1b7a2e17701cf9d43c
Dal Rupnik <legoless@gmail.com>
2017-03-04 16:29:04 +0100

    Added UMat wrapper

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVUMat.m
@@ -0,0 +1,13 @@
+//
+//  OCVUMat.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 04/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVUMat.h"
+
+@implementation OCVUMat
+
+@end
