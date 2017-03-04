1073da4f1ace4d8c7f331296e599e27510504254
Dal Rupnik <legoless@gmail.com>
2017-03-04 19:51:03 +0100

    Added OCVAlgorithm

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVAlgorithm.h
@@ -0,0 +1,13 @@
+//
+//  OCVAlgorithm.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 04/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVObject.h"
+
+@interface OCVAlgorithm : OCVObject
+
+@end
