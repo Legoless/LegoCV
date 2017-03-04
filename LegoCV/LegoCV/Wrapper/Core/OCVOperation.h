8476586ac70ac2c5e810d77c4f3f099c0e951653
Dal Rupnik <legoless@gmail.com>
2017-03-04 16:34:16 +0100

    Added few methods to operation

--- LegoCV/LegoCV/Wrapper/Core/OCVOperation.h
+++ LegoCV/LegoCV/Wrapper/Core/OCVOperation.h
@@ -16,6 +16,9 @@
 
 #import "OCVUMat.h"
 
+/*!
+ *  Wraps operations defined in core.hpp
+ */
 @interface OCVOperation : OCVObject
 
 + (void)swapMat:(OCVMat *)a toMat:(OCVMat *)b;
@@ -23,11 +26,21 @@
 
 + (NSInteger)borderInterpolateWithPixel:(NSInteger)pixel ofLength:(NSInteger)length withBorderType:(OCVBorderType)borderType;
 
+/*!
+ *  cv::copyMakeBorder()
+ */
 + (OCVScalar)copyMakeBorderFromSource:(id<OCVInputArrayable>)source toDestination:(id<OCVOutputArrayable>)destination withTop:(NSInteger)top withBottom:(NSInteger)bottom withLeft:(NSInteger)left withRight:(NSInteger)right withBorderType:(OCVBorderType)borderType;
 
 /*!
- *  Convenience method with custom struct
+ *  cv::copyMakeBorder() - Convenience method with custom struct
  */
 + (OCVScalar)copyMakeBorderFromSource:(id<OCVInputArrayable>)source toDestination:(id<OCVOutputArrayable>)destination withEdges:(OCVEdges)edges withBorderType:(OCVBorderType)borderType;
+
+/*!
+ *  cv::add()
+ */
++ (void)addSource:(id<OCVInputArrayable>)source1 withSource:(id<OCVInputArrayable>)source2 toDestination:(id<OCVOutputArrayable>)destination;
++ (void)addSource:(id<OCVInputArrayable>)source1 withSource:(id<OCVInputArrayable>)source2 toDestination:(id<OCVOutputArrayable>)destination withMask:(id<OCVInputArrayable>)mask;
++ (void)addSource:(id<OCVInputArrayable>)source1 withSource:(id<OCVInputArrayable>)source2 toDestination:(id<OCVOutputArrayable>)destination withMask:(id<OCVInputArrayable>)mask ofDepthType:(NSInteger)depthType;
 
 @end
