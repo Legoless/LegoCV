8072807595d13bafaeb8abab7b31b66d57cff7a6
Dal Rupnik <legoless@gmail.com>
2017-03-04 20:02:02 +0100

    Added methods to random generator

--- LegoCV/LegoCV/Wrapper/Core/OCVRandomGenerator.h
+++ LegoCV/LegoCV/Wrapper/Core/OCVRandomGenerator.h
@@ -8,6 +8,38 @@
 
 #import "OCVObject.h"
 
+#import "OCVInputArrayable.h"
+#import "OCVOutputArrayable.h"
+#import "OCVInputOutputArrayable.h"
+
+#import "OCVGeometry.h"
+#import "OCVTypes.h"
+
 @interface OCVRandomGenerator : OCVObject
 
+/*!
+ *  cv::theRNG()
+ */
++ (instancetype)defaultGenerator;
+
+/*!
+ *  cv::setRNGSeed()
+ */
++ (void)setSeed:(NSInteger)seed;
+
+/*!
+ *  cv::randu()
+ */
++ (void)randUniformToDestination:(id<OCVInputOutputArrayable>)destination withLowBoundary:(id<OCVInputArrayable>)lowBoundary withHighBoundary:(id<OCVInputArrayable>)highBoundary;
+
+/*!
+ *  cv::randn()
+ */
++ (void)randNormallyToDestination:(id<OCVInputOutputArrayable>)destination mean:(id<OCVInputArrayable>)mean standardDeviation:(id<OCVInputArrayable>)standardDeviation;
+
+/*!
+ *  cv::randShuffle()
+ */
+- (void)randShuffleToDestination:(id<OCVInputOutputArrayable>)destination;
+- (void)randShuffleToDestination:(id<OCVInputOutputArrayable>)destination iterationFactor:(double)iterationFactor;
 @end
