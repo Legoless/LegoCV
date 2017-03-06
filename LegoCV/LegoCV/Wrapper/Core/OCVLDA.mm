a00c61788bdb826feace7a0c1a5842f162fa7ab9
Dal Rupnik <legoless@gmail.com>
2017-03-07 00:12:51 +0100

    Added OCVLDA class

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVLDA.mm
@@ -0,0 +1,13 @@
+//
+//  OCVLDA.mm
+//  LegoCV
+//
+//  Created by Dal Rupnik on 07/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVLDA.h"
+
+@implementation OCVLDA
+
+@end
