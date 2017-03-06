810932a2014024c1c71293540fcaf159d07bc96b
Dal Rupnik <legoless@gmail.com>
2017-03-06 23:39:29 +0100

    Added OCVPCA

---
+++ LegoCV/LegoCV/Wrapper/Core/OCVPCA.h
@@ -0,0 +1,13 @@
+//
+//  OCVPCA.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 06/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVObject."
+
+@interface OCVPCA : OCVObject
+
+@end
---
+++ LegoCV/LegoCV/Wrapper/Core/OCVPCA.mm
@@ -0,0 +1,13 @@
+//
+//  OCVPCA.mm
+//  LegoCV
+//
+//  Created by Dal Rupnik on 06/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVPCA.h"
+
+@implementation OCVPCA
+
+@end
