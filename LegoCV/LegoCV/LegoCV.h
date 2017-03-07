16bd8ba8d87e68593e3a268bb67a04e261d56590
Dal Rupnik <legoless@gmail.com>
2017-03-07 01:17:09 +0100

    Created cocoapods compatible version

--- LegoCV/LegoCV/LegoCV.h
+++ LegoCV/LegoCV/LegoCV.h
@@ -16,5 +16,14 @@ FOUNDATION_EXPORT const unsigned char LegoCVVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LegoCV/PublicHeader.h>
 
+#if __has_include("OCVCore.h")
 #import "OCVCore.h"
+#endif
+
+#if __has_include("OCVImageProcessing.h")
+#import "OCVImageProcessing.h"
+#endif
+
+#if __has_include("OCVVideo.h")
 #import "OCVVideo.h"
+#endif
---
+++ LegoCV/LegoCV/Wrapper/ImageProcessing/OCVImageProcessing.h
@@ -0,0 +1,14 @@
+//
+//  OCVImageProcessing.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 07/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#ifndef OCVImageProcessing_h
+#define OCVImageProcessing_h
+
+#import "OCVImageOperation.h"
+
+#endif /* OCVImageProcessing_h */
