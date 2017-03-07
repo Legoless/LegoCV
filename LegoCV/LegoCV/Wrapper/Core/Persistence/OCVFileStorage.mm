855a6d795dfb2d9daad02dcbbef7e40fe2e67d5f
Dal Rupnik <legoless@gmail.com>
2017-03-07 19:51:41 +0100

    Implemented file format descriptor

--- LegoCV/LegoCV/Wrapper/Core/Persistence/OCVFileStorage.mm
+++ LegoCV/LegoCV/Wrapper/Core/Persistence/OCVFileStorage.mm
@@ -8,6 +8,67 @@
 
 #import "OCVFileStorage.h"
 
+NSString* NSStringFromFileStorageFormatType(OCVFileStorageFormatType type) {
+    switch (type) {
+        case OCVFileStorageFormatType8BitUnsigned:
+            return @"u";
+        case OCVFileStorageFormatType8BitSigned:
+            return @"c";
+        case OCVFileStorageFormatType16BitUnsigned:
+            return @"w";
+        case OCVFileStorageFormatType16BitSigned:
+            return @"s";
+        case OCVFileStorageFormatType32BitSigned:
+            return @"i";
+        case OCVFileStorageFormatTypeSinglePrecisionFloat:
+            return @"f";
+        case OCVFileStorageFormatTypeDoublePrecisionFloat:
+            return @"d";
+        case OCVFileStorageFormatType32BitPointer:
+            return @"r";
+        default:
+            return @"";
+    }
+}
+
+@interface OCVFileStorageFormatDescriptor ()
+
+@property (nonatomic, strong) NSMutableString *formatStore;
+
+@end
+
+@implementation OCVFileStorageFormatDescriptor
+
+#pragma mark - Properties
+
+- (NSMutableString *)formatStore {
+    if (!_formatStore) {
+        _formatStore = [[NSMutableString alloc] init];
+    }
+    
+    return _formatStore;
+}
+
+- (NSString *)format {
+    return [self.formatStore copy];
+}
+
+#pragma mark - Public Methods
+
+- (void)addFormatOfType:(OCVFileStorageFormatType)type {
+    [self.formatStore appendString:NSStringFromFileStorageFormatType(type)];
+}
+
+- (void)addFormatOfType:(OCVFileStorageFormatType)type withCount:(NSInteger)count {
+    if (count > 1) {
+        [self.formatStore appendFormat:@"%ldd", (long)count];
+    }
+    
+    [self.formatStore appendString:NSStringFromFileStorageFormatType(type)];
+}
+
+@end
+
 @implementation OCVFileStorage
 
 @end
