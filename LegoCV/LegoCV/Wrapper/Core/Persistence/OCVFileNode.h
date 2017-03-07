110941fbfed8508a82ee7b8cdc0a4a026b9c70b1
Dal Rupnik <legoless@gmail.com>
2017-03-07 19:20:33 +0100

    Added OCVFileNode

---
+++ LegoCV/LegoCV/Wrapper/Core/Persistence/OCVFileNode.h
@@ -0,0 +1,13 @@
+//
+//  OCVFileNode.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 07/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import <LegoCV/LegoCV.h>
+
+@interface OCVFileNode : OCVObject
+
+@end
---
+++ LegoCV/LegoCV/Wrapper/Core/Persistence/OCVFileNode.m
@@ -0,0 +1,13 @@
+//
+//  OCVFileNode.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 07/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVFileNode.h"
+
+@implementation OCVFileNode
+
+@end
