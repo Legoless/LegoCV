b48a38bc549094333a5f356d34118a9547985010
Dal Rupnik <legoless@gmail.com>
2017-03-07 19:19:48 +0100

    Added OCVFileStorage

---
+++ LegoCV/LegoCV/Wrapper/Core/Persistence/OCVFileStorage.h
@@ -0,0 +1,13 @@
+//
+//  OCVFileStorage.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 07/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVObject.h"
+
+@interface OCVFileStorage : OCVObject
+
+@end
---
+++ LegoCV/LegoCV/Wrapper/Core/Persistence/OCVFileStorage.m
@@ -0,0 +1,13 @@
+//
+//  OCVFileStorage.m
+//  LegoCV
+//
+//  Created by Dal Rupnik on 07/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVFileStorage.h"
+
+@implementation OCVFileStorage
+
+@end
