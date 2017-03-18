f1aa6e3efde767289d1c2b28224b80cf930810dd
Dal Rupnik <legoless@gmail.com>
2017-03-18 15:08:32 +0100

    Finished header of generalized hough guil

---
+++ LegoCV/LegoCV/Wrapper/ImageProcessing/OCVGeneralizedHoughGuil.h
@@ -0,0 +1,25 @@
+//
+//  OCVGeneralizedHoughGuil.h
+//  LegoCV
+//
+//  Created by Dal Rupnik on 18/03/2017.
+//  Copyright © 2017 Unified Sense. All rights reserved.
+//
+
+#import "OCVGeneralizedHough.h"
+
+@interface OCVGeneralizedHoughGuil : OCVGeneralizedHough
+
+@property (nonatomic, assign) double xi;
+@property (nonatomic, assign) NSInteger levels;
+@property (nonatomic, assign) double angleEpsilon;
+@property (nonatomic, assign) double minAngle;
+@property (nonatomic, assign) double maxAngle;
+@property (nonatomic, assign) double angleStep;
+@property (nonatomic, assign) NSInteger angleThreshold;
+@property (nonatomic, assign) double maxScale;
+@property (nonatomic, assign) double scaleStep;
+@property (nonatomic, assign) NSInteger scaleThreshold;
+@property (nonatomic, assign) NSInteger positionThreshold;
+
+@end
