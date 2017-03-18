9d637ab3a07924edc2d7f089af3b302a6f676972
Dal Rupnik <legoless@gmail.com>
2017-03-18 14:26:01 +0100

    Added interpolation masks

--- LegoCV/LegoCV/Wrapper/ImageProcessing/OCVInterpolationType.h
+++ LegoCV/LegoCV/Wrapper/ImageProcessing/OCVInterpolationType.h
@@ -9,12 +9,29 @@
 #ifndef OCVInterpolationType_h
 #define OCVInterpolationType_h
 
-typedef NS_ENUM(NSInteger, OCVInterpolationType) {
-    OCVInterpolationTypeNearest     = 0,
-    OCVInterpolationTypeLinear      = 1,
-    OCVInterpolationTypeCubic       = 2,
-    OCVInterpolationTypeArea        = 3,
-    OCVInterpolationTypeLanczos4    = 4
+/*!
+ *  Reference type: cv::InterpolationFlags
+ */
+typedef NS_OPTIONS(NSInteger, OCVInterpolationType) {
+    OCVInterpolationTypeNearest             = 0,
+    OCVInterpolationTypeLinear              = 1,
+    OCVInterpolationTypeCubic               = 2,
+    OCVInterpolationTypeArea                = 3,
+    OCVInterpolationTypeLanczos4            = 4,
+    OCVInterpolationTypeMax                 = 7,
+    OCVInterpolationTypeWarpFillOutliers    = 8,
+    OCVInterpolationTypeWarpInverseMap      = 9
 };
+
+/*!
+ *  Reference type: cv::InterpolationMasks
+ */
+typedef NS_OPTIONS(NSInteger, OCVInterpolationMasks) {
+    OCVInterpolationMaskBits      = 5,
+    OCVInterpolationMaskBits2     = OCVInterpolationMaskBits * 2,
+    OCVInterpolationMaskTabSize   = 1 << OCVInterpolationMaskBits,
+    OCVInterpolationMaskTabSize2  = OCVInterpolationMaskTabSize * OCVInterpolationMaskTabSize
+};
+
 
 #endif /* OCVInterpolationType_h */
