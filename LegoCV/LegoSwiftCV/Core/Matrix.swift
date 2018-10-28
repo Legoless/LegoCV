dfaca3631d21e66b7976e92179bd46072e2e05dd
Dal Rupnik <legoless@gmail.com>
2018-10-28 20:01:50 +0100

    Add LegoSwift extensions framework

---
+++ LegoCV/LegoSwiftCV/Core/Matrix.swift
@@ -0,0 +1,14 @@
+//
+//  Matrix.swift
+//  LegoCV
+//
+//  Created by Dal Rupnik on 10/28/18.
+//  Copyright © 2018 Unified Sense. All rights reserved.
+//
+
+import Foundation
+import LegoCV
+
+public class Matrix<T : Numeric> : OCVMat {
+    
+}
---
+++ LegoCV/LegoSwiftCV/LegoSwiftCV.h
@@ -0,0 +1,19 @@
+//
+//  LegoSwiftCV.h
+//  LegoSwiftCV
+//
+//  Created by Dal Rupnik on 10/28/18.
+//  Copyright © 2018 Unified Sense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LegoSwiftCV.
+FOUNDATION_EXPORT double LegoSwiftCVVersionNumber;
+
+//! Project version string for LegoSwiftCV.
+FOUNDATION_EXPORT const unsigned char LegoSwiftCVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LegoSwiftCV/PublicHeader.h>
+
+
